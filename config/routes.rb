fc3c2c3655781e17bb978f6045ce8b23a2c1778e
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-18 11:51:11 -0700

    add / as route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/movies', to: 'movies#index'
+      get '/', to: 'movies#index'
       get '/movies/index', to: 'movies#index'
       get '/movies/:id', to: 'movies#show'
       post '/movies', to: 'movies#create'
