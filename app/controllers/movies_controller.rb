51144f82883f3bcd1839ccca7dda69538628a216
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-17 19:01:56 -0700

    add create update and destroy actions, utilize with postman

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,4 +12,40 @@ class MoviesController < ApplicationController
 
   end
 
+  def create
+
+    @movie = Movie.new(
+      name: params[:name],
+      length: params[:length],
+      description: params[:description],
+      year: params[:year]
+      )
+
+    @movie.save
+    render :show
+
+  end
+
+  def update
+
+    @movie = Movie.find_by(id: params[:id])
+    @movie.assign_attributes(
+      name: params[:name],
+      length: params[:length],
+      description: params[:description],
+      year: params[:year]
+      )
+    @movie.save
+    render :show
+
+  end
+
+  def destroy
+
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    render :show
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/movies', to: 'movies#index'
   get '/movies/index', to: 'movies#index'
   get '/movies/:id', to: 'movies#show'
+  post '/movies', to: 'movies#create'
+  patch '/movies/:id', to: 'movies#update'
+  delete '/movies/:id', to: 'movies#destroy'
 
 end
